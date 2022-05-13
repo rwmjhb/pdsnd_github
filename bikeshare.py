76bbb1c8ae3da33a92396a68cb4e708175551e8d
rwmjhb <rwmjhb@hotmail.com>
2022-05-13 10:35:28 +0800

    Fix some description

--- bikeshare.py
+++ bikeshare.py
@@ -1,4 +1,3 @@
-import time
 import pandas as pd
 import numpy as np
 
@@ -11,9 +10,9 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) load_city - name of the city to analyze
+        (str) load_month - name of the month to filter by, or "all" to apply no month filter
+        (str) load_day - name of the day of week to filter by, or "all" to apply no day filter
     """
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     inputcityx=True
