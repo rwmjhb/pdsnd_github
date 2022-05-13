d90859a24f8b6ac70ff9acb0b32421bc6840ecb8
rwmjhb <rwmjhb@hotmail.com>
2022-05-13 10:44:36 +0800

    Fix some description

--- bikeshare.py
+++ bikeshare.py
@@ -50,9 +50,9 @@ def  load_date(load_city,load_month,load_day):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) load_city - name of the city to analyze
+        (str) load_month - name of the month to filter by, or "all" to apply no month filter
+        (str) load_day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
